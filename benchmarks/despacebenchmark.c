de8f9e3accb8e940cb2a489a3ab8f2ffb253b22b
Daniel Lemire <lemire@gmail.com>
2017-01-20 11:05:16 -0500

    Reformat.

--- benchmarks/despacebenchmark.c
+++ benchmarks/despacebenchmark.c
@@ -4,57 +4,58 @@
 #include <sys/time.h>
 #include "despacer.h"
 
-#define RDTSC_START(cycles)                                                   \
-    do {                                                                      \
-        register unsigned cyc_high, cyc_low;                                  \
-        __asm volatile(                                                       \
-            "cpuid\n\t"                                                       \
-            "rdtsc\n\t"                                                       \
-            "mov %%edx, %0\n\t"                                               \
-            "mov %%eax, %1\n\t"                                               \
-            : "=r"(cyc_high), "=r"(cyc_low)::"%rax", "%rbx", "%rcx", "%rdx"); \
-        (cycles) = ((uint64_t)cyc_high << 32) | cyc_low;                      \
-    } while (0)
+#define RDTSC_START(cycles)                                                    \
+  do {                                                                         \
+    register unsigned cyc_high, cyc_low;                                       \
+    __asm volatile("cpuid\n\t"                                                 \
+                   "rdtsc\n\t"                                                 \
+                   "mov %%edx, %0\n\t"                                         \
+                   "mov %%eax, %1\n\t"                                         \
+                   : "=r"(cyc_high), "=r"(cyc_low)::"%rax", "%rbx", "%rcx",    \
+                     "%rdx");                                                  \
+    (cycles) = ((uint64_t)cyc_high << 32) | cyc_low;                           \
+  } while (0)
 
-#define RDTSC_FINAL(cycles)                                                   \
-    do {                                                                      \
-        register unsigned cyc_high, cyc_low;                                  \
-        __asm volatile(                                                       \
-            "rdtscp\n\t"                                                      \
-            "mov %%edx, %0\n\t"                                               \
-            "mov %%eax, %1\n\t"                                               \
-            "cpuid\n\t"                                                       \
-            : "=r"(cyc_high), "=r"(cyc_low)::"%rax", "%rbx", "%rcx", "%rdx"); \
-        (cycles) = ((uint64_t)cyc_high << 32) | cyc_low;                      \
-    } while (0)
+#define RDTSC_FINAL(cycles)                                                    \
+  do {                                                                         \
+    register unsigned cyc_high, cyc_low;                                       \
+    __asm volatile("rdtscp\n\t"                                                \
+                   "mov %%edx, %0\n\t"                                         \
+                   "mov %%eax, %1\n\t"                                         \
+                   "cpuid\n\t"                                                 \
+                   : "=r"(cyc_high), "=r"(cyc_low)::"%rax", "%rbx", "%rcx",    \
+                     "%rdx");                                                  \
+    (cycles) = ((uint64_t)cyc_high << 32) | cyc_low;                           \
+  } while (0)
 
-#define BEST_TIME_CHECK(test, check, pre, repeat, number)                         \
-    do {                                                              \
-        printf("%s: ", #test);                                        \
-        fflush(NULL);                                                 \
-        uint64_t tm1, tm2;                               \
-        uint64_t min_diff = (uint64_t)-1;                             \
-        for (int i = 0; i < repeat; i++) {                            \
-            pre;                                                     \
-            __asm volatile("" ::: /* pretend to clobber */ "memory"); \
-            RDTSC_START(tm1);                                \
-            if( test != check) printf("bug");                                                     \
-            RDTSC_FINAL(tm2);                                \
-            uint64_t tmus = tm2- tm1 ;           \
-            if (tmus < min_diff) min_diff = tmus;       \
-        }                                                             \
-        printf(" %f cycles / ops", min_diff * 1.0 / number);           \
-        printf("\n");                                                 \
-        fflush(NULL);                                                 \
-    } while (0)
-
+#define BEST_TIME_CHECK(test, check, pre, repeat, number)                      \
+  do {                                                                         \
+    printf("%s: ", #test);                                                     \
+    fflush(NULL);                                                              \
+    uint64_t tm1, tm2;                                                         \
+    uint64_t min_diff = (uint64_t)-1;                                          \
+    for (int i = 0; i < repeat; i++) {                                         \
+      pre;                                                                     \
+      __asm volatile("" ::: /* pretend to clobber */ "memory");                \
+      RDTSC_START(tm1);                                                        \
+      if (test != check)                                                       \
+        printf("bug");                                                         \
+      RDTSC_FINAL(tm2);                                                        \
+      uint64_t tmus = tm2 - tm1;                                               \
+      if (tmus < min_diff)                                                     \
+        min_diff = tmus;                                                       \
+    }                                                                          \
+    printf(" %f cycles / ops", min_diff * 1.0 / number);                       \
+    printf("\n");                                                              \
+    fflush(NULL);                                                              \
+  } while (0)
 
 // let us estimate that we have a 1% proba of hitting a white space
-size_t fillwithtext(char * buffer, size_t size) {
+size_t fillwithtext(char *buffer, size_t size) {
   size_t howmany = 0;
-  for(size_t i = 0; i < size; ++i) {
-    double r = ((double) rand() / (RAND_MAX));
-    if(r < 0.01) {
+  for (size_t i = 0; i < size; ++i) {
+    double r = ((double)rand() / (RAND_MAX));
+    if (r < 0.01) {
       buffer[i] = ' ';
       howmany++;
     } else if (r < 0.02) {
@@ -65,29 +66,33 @@ size_t fillwithtext(char * buffer, size_t size) {
       howmany++;
     } else {
       do {
-        buffer[i] = (char) rand();
-      } while (buffer[i] == '\r' || buffer[i] == '\n' || buffer[i] == ' ') ;
+        buffer[i] = (char)rand();
+      } while (buffer[i] == '\r' || buffer[i] == '\n' || buffer[i] == ' ');
     }
   }
   return howmany;
 }
 
-
 int main() {
   const int N = 1024;
-  char * buffer = malloc(N);
-  char * tmpbuffer = malloc(N);
+  char *buffer = malloc(N);
+  char *tmpbuffer = malloc(N);
   int repeat = 100;
   size_t howmanywhite;
 
-  BEST_TIME_CHECK(despace(buffer, N), N-howmanywhite, howmanywhite = fillwithtext(buffer, N) ,repeat, N);
-  BEST_TIME_CHECK(despace_to(buffer, N, tmpbuffer), N-howmanywhite, howmanywhite = fillwithtext(buffer, N) ,repeat, N);
+  BEST_TIME_CHECK(despace(buffer, N), N - howmanywhite,
+                  howmanywhite = fillwithtext(buffer, N), repeat, N);
+  BEST_TIME_CHECK(despace_to(buffer, N, tmpbuffer), N - howmanywhite,
+                  howmanywhite = fillwithtext(buffer, N), repeat, N);
 #ifdef __AVX2__
-  BEST_TIME_CHECK(avx2_despace(buffer, N), N-howmanywhite, howmanywhite = fillwithtext(buffer, N) ,repeat, N);
+  BEST_TIME_CHECK(avx2_despace(buffer, N), N - howmanywhite,
+                  howmanywhite = fillwithtext(buffer, N), repeat, N);
 #endif
 #ifdef __SSE4_1__
-  BEST_TIME_CHECK(sse4_despace(buffer, N), N-howmanywhite, howmanywhite = fillwithtext(buffer, N) ,repeat, N);
-  BEST_TIME_CHECK(sse4_despace_trail(buffer, N), N-howmanywhite, howmanywhite = fillwithtext(buffer, N) ,repeat, N);
+  BEST_TIME_CHECK(sse4_despace(buffer, N), N - howmanywhite,
+                  howmanywhite = fillwithtext(buffer, N), repeat, N);
+  BEST_TIME_CHECK(sse4_despace_trail(buffer, N), N - howmanywhite,
+                  howmanywhite = fillwithtext(buffer, N), repeat, N);
 
 #endif
   free(buffer);
--- include/despacer.h
+++ include/despacer.h
@@ -7,83 +7,93 @@
 #include "despacer_tables.h"
 
 /**
-* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing "howmany"
-* characters (not counting the null character if a C string is used), returns the new number of characters
-* this function does add the null character, you have to do it yourself if you need it.
+* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing
+* "howmany"
+* characters (not counting the null character if a C string is used), returns
+* the new number of characters
+* this function does add the null character, you have to do it yourself if you
+* need it.
 */
-static inline size_t despace(char * bytes, size_t howmany) {
+static inline size_t despace(char *bytes, size_t howmany) {
   size_t pos = 0;
-  for(size_t i = 0; i < howmany; i++) {
-      char c = bytes[i];
-      if (c == '\r' || c == '\n' || c == ' ') {
-        continue;
-      }
-      bytes[pos++] = c;
+  for (size_t i = 0; i < howmany; i++) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      continue;
+    }
+    bytes[pos++] = c;
   }
   return pos;
 }
 
 /**
 * remove spaces from string bytes (UTF-8 or ASCII) containing "howmany"
-* characters (not counting the null character if a C string is used), returns the new number of characters
-* this function does add the null character, you have to do it yourself if you need it.
-* Result is stored in "out", where the caller is responsible for memory allocation.
+* characters (not counting the null character if a C string is used), returns
+* the new number of characters
+* this function does add the null character, you have to do it yourself if you
+* need it.
+* Result is stored in "out", where the caller is responsible for memory
+* allocation.
 */
-static inline size_t despace_to(const char * __restrict__ bytes, size_t howmany, char * __restrict__ out) {
+static inline size_t despace_to(const char *__restrict__ bytes, size_t howmany,
+                                char *__restrict__ out) {
   size_t pos = 0;
-  for(size_t i = 0; i < howmany; i++) {
-      char c = bytes[i];
-      if (c == '\r' || c == '\n' || c == ' ') {
-        continue;
-      }
-      out[pos++] = c;
+  for (size_t i = 0; i < howmany; i++) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      continue;
+    }
+    out[pos++] = c;
   }
   return pos;
 }
-
 
 #ifdef __AVX2__
 
 #include <x86intrin.h>
 
 /**
-* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing "howmany"
-* characters (not counting the null character if a C string is used), returns the new number of characters
-* this function does add the null character, you have to do it yourself if you need it.
+* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing
+* "howmany"
+* characters (not counting the null character if a C string is used), returns
+* the new number of characters
+* this function does add the null character, you have to do it yourself if you
+* need it.
 */
-static inline size_t avx2_despace(char * bytes, size_t howmany) {
+static inline size_t avx2_despace(char *bytes, size_t howmany) {
   size_t pos = 0;
   __m256i spaces = _mm256_set1_epi8(' ');
   __m256i newline = _mm256_set1_epi8('\n');
   __m256i carriage = _mm256_set1_epi8('\r');
   size_t i = 0;
-  for(; i + 31 < howmany; i+=32) {
-      __m256i x = _mm256_loadu_si256((const __m256i *) (bytes + i));
-      // we do it the naive way, could be smarter?
-      __m256i xspaces = _mm256_cmpeq_epi8(x,spaces);
-      __m256i xnewline = _mm256_cmpeq_epi8(x,newline);
-      __m256i xcarriage = _mm256_cmpeq_epi8(x,carriage);
-      __m256i anywhite = _mm256_or_si256(_mm256_or_si256(xspaces,xnewline),xcarriage);
-      if(_mm256_testz_si256(anywhite,anywhite) == 1) { // no white space
-        _mm256_storeu_si256((__m256i *) (bytes + pos),x);
-        pos += 32;
-      } else {
-        // do it the hard way
-        for(size_t j = 0; j < 32; j++) {
-          char c = bytes[i + j];
-          if (c == '\r' || c == '\n' || c == ' ') {
-            continue;
-          }
-          bytes[pos++] = c;
+  for (; i + 31 < howmany; i += 32) {
+    __m256i x = _mm256_loadu_si256((const __m256i *)(bytes + i));
+    // we do it the naive way, could be smarter?
+    __m256i xspaces = _mm256_cmpeq_epi8(x, spaces);
+    __m256i xnewline = _mm256_cmpeq_epi8(x, newline);
+    __m256i xcarriage = _mm256_cmpeq_epi8(x, carriage);
+    __m256i anywhite =
+        _mm256_or_si256(_mm256_or_si256(xspaces, xnewline), xcarriage);
+    if (_mm256_testz_si256(anywhite, anywhite) == 1) { // no white space
+      _mm256_storeu_si256((__m256i *)(bytes + pos), x);
+      pos += 32;
+    } else {
+      // do it the hard way
+      for (size_t j = 0; j < 32; j++) {
+        char c = bytes[i + j];
+        if (c == '\r' || c == '\n' || c == ' ') {
+          continue;
         }
+        bytes[pos++] = c;
       }
     }
-  for(; i < howmany; i++) {
-      char c = bytes[i];
-      if (c == '\r' || c == '\n' || c == ' ') {
-        continue;
-      }
-      bytes[pos++] = c;
+  }
+  for (; i < howmany; i++) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      continue;
+    }
+    bytes[pos++] = c;
   }
   return pos;
 }
@@ -95,83 +105,90 @@ static inline size_t avx2_despace(char * bytes, size_t howmany) {
 #include <x86intrin.h>
 
 /**
-* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing "howmany"
-* characters (not counting the null character if a C string is used), returns the new number of characters
-* this function does add the null character, you have to do it yourself if you need it.
+* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing
+* "howmany"
+* characters (not counting the null character if a C string is used), returns
+* the new number of characters
+* this function does add the null character, you have to do it yourself if you
+* need it.
 */
-static inline size_t sse4_despace(char * bytes, size_t howmany) {
+static inline size_t sse4_despace(char *bytes, size_t howmany) {
   size_t pos = 0;
   __m128i spaces = _mm_set1_epi8(' ');
   __m128i newline = _mm_set1_epi8('\n');
   __m128i carriage = _mm_set1_epi8('\r');
   size_t i = 0;
-  for(; i + 15 < howmany; i+=16) {
-      __m128i x = _mm_loadu_si128((const __m128i *) (bytes + i));
-      // we do it the naive way, could be smarter?
-      __m128i xspaces = _mm_cmpeq_epi8(x,spaces);
-      __m128i xnewline = _mm_cmpeq_epi8(x,newline);
-      __m128i xcarriage = _mm_cmpeq_epi8(x,carriage);
-      __m128i anywhite = _mm_or_si128(_mm_or_si128(xspaces,xnewline),xcarriage);
-      int mask16 = _mm_movemask_epi8(anywhite);
-      if(mask16 == 0) {// no match!
-        _mm_storeu_si128((__m128i *) (bytes + pos),x);
-        pos += 16;
-      } else {
-        x = _mm_shuffle_epi8(x,_mm_loadu_si128((const __m128i *) despace_mask16 + mask16));
-        _mm_storeu_si128((__m128i *) (bytes + pos),x);
-        pos += 16 - _mm_popcnt_u32(mask16);
-      }
+  for (; i + 15 < howmany; i += 16) {
+    __m128i x = _mm_loadu_si128((const __m128i *)(bytes + i));
+    // we do it the naive way, could be smarter?
+    __m128i xspaces = _mm_cmpeq_epi8(x, spaces);
+    __m128i xnewline = _mm_cmpeq_epi8(x, newline);
+    __m128i xcarriage = _mm_cmpeq_epi8(x, carriage);
+    __m128i anywhite = _mm_or_si128(_mm_or_si128(xspaces, xnewline), xcarriage);
+    int mask16 = _mm_movemask_epi8(anywhite);
+    if (mask16 == 0) { // no match!
+      _mm_storeu_si128((__m128i *)(bytes + pos), x);
+      pos += 16;
+    } else {
+      x = _mm_shuffle_epi8(
+          x, _mm_loadu_si128((const __m128i *)despace_mask16 + mask16));
+      _mm_storeu_si128((__m128i *)(bytes + pos), x);
+      pos += 16 - _mm_popcnt_u32(mask16);
     }
-  for(; i < howmany; i++) {
-      char c = bytes[i];
-      if (c == '\r' || c == '\n' || c == ' ') {
-        continue;
-      }
-      bytes[pos++] = c;
+  }
+  for (; i < howmany; i++) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      continue;
+    }
+    bytes[pos++] = c;
   }
   return pos;
 }
 
 /**
-* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing "howmany"
-* characters (not counting the null character if a C string is used), returns the new number of characters
-* this function does add the null character, you have to do it yourself if you need it.
+* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing
+* "howmany"
+* characters (not counting the null character if a C string is used), returns
+* the new number of characters
+* this function does add the null character, you have to do it yourself if you
+* need it.
 */
-static inline size_t sse4_despace_trail(char * bytes, size_t howmany) {
+static inline size_t sse4_despace_trail(char *bytes, size_t howmany) {
   size_t pos = 0;
   __m128i spaces = _mm_set1_epi8(' ');
   __m128i newline = _mm_set1_epi8('\n');
   __m128i carriage = _mm_set1_epi8('\r');
   size_t i = 0;
-  for(; i + 15 < howmany; i+=16) {
-      __m128i x = _mm_loadu_si128((const __m128i *) (bytes + i));
-      // we do it the naive way, could be smarter?
-      __m128i xspaces = _mm_cmpeq_epi8(x,spaces);
-      __m128i xnewline = _mm_cmpeq_epi8(x,newline);
-      __m128i xcarriage = _mm_cmpeq_epi8(x,carriage);
-      __m128i anywhite = _mm_or_si128(_mm_or_si128(xspaces,xnewline),xcarriage);
-      int mask16 = _mm_movemask_epi8(anywhite);
-      if(mask16 == 0) {// no match!
-        _mm_storeu_si128((__m128i *) (bytes + pos),x);
-        pos += 16;
-      } else {
-        for(int k = 0; k < 16; ++k) {
-          if(mask16 & (1<<k)) continue;
-          bytes[pos++] = bytes[i + k];
-        }
+  for (; i + 15 < howmany; i += 16) {
+    __m128i x = _mm_loadu_si128((const __m128i *)(bytes + i));
+    // we do it the naive way, could be smarter?
+    __m128i xspaces = _mm_cmpeq_epi8(x, spaces);
+    __m128i xnewline = _mm_cmpeq_epi8(x, newline);
+    __m128i xcarriage = _mm_cmpeq_epi8(x, carriage);
+    __m128i anywhite = _mm_or_si128(_mm_or_si128(xspaces, xnewline), xcarriage);
+    int mask16 = _mm_movemask_epi8(anywhite);
+    if (mask16 == 0) { // no match!
+      _mm_storeu_si128((__m128i *)(bytes + pos), x);
+      pos += 16;
+    } else {
+      for (int k = 0; k < 16; ++k) {
+        if (mask16 & (1 << k))
+          continue;
+        bytes[pos++] = bytes[i + k];
       }
     }
-  for(; i < howmany; i++) {
-      char c = bytes[i];
-      if (c == '\r' || c == '\n' || c == ' ') {
-        continue;
-      }
-      bytes[pos++] = c;
+  }
+  for (; i < howmany; i++) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      continue;
+    }
+    bytes[pos++] = c;
   }
   return pos;
 }
 
 #endif
-
 
 #endif
