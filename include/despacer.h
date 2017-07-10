dedc0b98a476b25ea005e65b151b0cfbdd83c780
Daniel Lemire <lemire@gmail.com>
2017-07-10 14:04:51 -0400

    Moving code around to address https://github.com/lemire/despacer/issues/5

--- include/despacer.h
+++ include/despacer.h
@@ -160,101 +160,6 @@ static inline size_t despace_to(const char *__restrict__ bytes, size_t howmany,
   return pos;
 }
 
-#ifdef __AVX2__
-
-#include <x86intrin.h>
-
-
-#ifndef __clang__
-static inline __m256i _mm256_loadu2_m128i(__m128i const *__addr_hi, __m128i const *__addr_lo) {
-  __m256i __v256 = _mm256_castsi128_si256(_mm_loadu_si128(__addr_lo));
-  return _mm256_insertf128_si256(__v256, _mm_loadu_si128(__addr_hi), 1);
-}
-
-static inline void _mm256_storeu2_m128i(__m128i *__addr_hi, __m128i *__addr_lo, __m256i __a) {
-  __m128i __v128;
-
-  __v128 = _mm256_castsi256_si128(__a);
-  _mm_storeu_si128(__addr_lo, __v128);
-  __v128 = _mm256_extractf128_si256(__a, 1);
-  _mm_storeu_si128(__addr_hi, __v128);
-}
-#endif
-
-/**
-* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing
-* "howmany"
-* characters (not counting the null character if a C string is used), returns
-* the new number of characters
-* this function does add the null character, you have to do it yourself if you
-* need it.
-*/
-static inline size_t avx2_despace(char *bytes, size_t howmany) {
-  size_t pos = 0;
-  __m256i spaces = _mm256_set1_epi8(' ');
-  __m256i newline = _mm256_set1_epi8('\n');
-  __m256i carriage = _mm256_set1_epi8('\r');
-  size_t i = 0;
-  for (; i + 31 < howmany; i += 32) {
-    __m256i x = _mm256_loadu_si256((const __m256i *)(bytes + i));
-    // we do it the naive way, could be smarter?
-    __m256i xspaces = _mm256_cmpeq_epi8(x, spaces);
-    __m256i xnewline = _mm256_cmpeq_epi8(x, newline);
-    __m256i xcarriage = _mm256_cmpeq_epi8(x, carriage);
-    __m256i anywhite =
-        _mm256_or_si256(_mm256_or_si256(xspaces, xnewline), xcarriage);
-    if (_mm256_testz_si256(anywhite, anywhite) == 1) { // no white space
-      _mm256_storeu_si256((__m256i *)(bytes + pos), x);
-      pos += 32;
-    } else {
-      // do it the hard way
-      for (size_t j = 0; j < 32; j++) {
-        char c = bytes[i + j];
-        if (c == '\r' || c == '\n' || c == ' ') {
-          continue;
-        }
-        bytes[pos++] = c;
-      }
-    }
-  }
-  for (; i < howmany; i++) {
-    char c = bytes[i];
-    if (c == '\r' || c == '\n' || c == ' ') {
-      continue;
-    }
-    bytes[pos++] = c;
-  }
-  return pos;
-}
-
-static inline size_t avx2_countspaces(const char *bytes, size_t howmany) {
-  size_t count = 0;
-  __m256i spaces = _mm256_set1_epi8(' ');
-  __m256i newline = _mm256_set1_epi8('\n');
-  __m256i carriage = _mm256_set1_epi8('\r');
-  size_t i = 0;
-  for (; i + 31 < howmany; i += 32) {
-    __m256i x = _mm256_loadu_si256((const __m256i *)(bytes + i));
-    // we do it the naive way, could be smarter?
-    __m256i xspaces = _mm256_cmpeq_epi8(x, spaces);
-    __m256i xnewline = _mm256_cmpeq_epi8(x, newline);
-    __m256i xcarriage = _mm256_cmpeq_epi8(x, carriage);
-    __m256i anywhite =
-        _mm256_or_si256(_mm256_or_si256(xspaces, xnewline), xcarriage);
-    count += _mm_popcnt_u32(_mm256_movemask_epi8(anywhite));
-  }
-  for (; i < howmany; i++) {
-    char c = bytes[i];
-    if (c == '\r' || c == '\n' || c == ' ') {
-      count ++ ;
-    }
-  }
-  return count;
-}
-
-
-#endif
-
 #ifdef __SSE4_1__
 
 #include <x86intrin.h>
@@ -362,121 +267,6 @@ static inline __m128i cleanm128(__m128i x, __m128i spaces, __m128i newline, __m1
 }
 
 
-static inline __m256i cleanm256(__m256i x, __m256i spaces, __m256i newline, __m256i carriage, unsigned int * mask1, unsigned int *mask2) {
-    __m256i xspaces = _mm256_cmpeq_epi8(x, spaces);
-    __m256i xnewline = _mm256_cmpeq_epi8(x, newline);
-    __m256i xcarriage = _mm256_cmpeq_epi8(x, carriage);
-    __m256i anywhite = _mm256_or_si256(_mm256_or_si256(xspaces, xnewline), xcarriage);
-    unsigned int mask32 = _mm256_movemask_epi8(anywhite);
-    unsigned int maskhigh = (mask32) >> 16;
-    unsigned int masklow = (mask32) & 0xFFFF;
-    assert(maskhigh<(1<<16));
-    assert(masklow<(1<<16));
-    *mask1 = masklow;
-    *mask2 = maskhigh;
-    __m256i mask = _mm256_loadu2_m128i((const __m128i *)despace_mask16 + maskhigh, (const __m128i *)despace_mask16 + masklow);
-    return _mm256_shuffle_epi8(
-          x, mask);
-
-}
-
-static inline size_t avx2_despace_branchless(char *bytes, size_t howmany) {
-  size_t pos = 0;
-  __m128i spaces = _mm_set1_epi8(' ');
-  __m128i newline = _mm_set1_epi8('\n');
-  __m128i carriage = _mm_set1_epi8('\r');
-
-  __m256i spaces256 = _mm256_set1_epi8(' ');
-  __m256i newline256 = _mm256_set1_epi8('\n');
-  __m256i carriage256 = _mm256_set1_epi8('\r');
-
-  size_t i = 0;
-  for (; i + 32 - 1 < howmany; i += 32) {
-    __m256i x = _mm256_loadu_si256((const __m256i *)(bytes + i));
-    unsigned int masklow, maskhigh;
-    x = cleanm256(x,spaces256,newline256,carriage256, & masklow, & maskhigh);
-    int offset1 = 16 - _mm_popcnt_u32(masklow);
-    int offset2 = 16 - _mm_popcnt_u32(maskhigh);
-    _mm256_storeu2_m128i((__m128i *)(bytes + pos + offset1), (__m128i *)(bytes + pos ),x);
-    pos += offset1 + offset2;
-  }
-  for (; i + 16 - 1 < howmany; i += 16) {
-    __m128i x = _mm_loadu_si128((const __m128i *)(bytes + i));
-    int mask16;
-    x = cleanm128(x,spaces,newline,carriage, & mask16);
-    _mm_storeu_si128((__m128i *)(bytes + pos), x);
-    pos += 16 - _mm_popcnt_u32(mask16);
-  }
-  for (; i < howmany; i++) {
-    char c = bytes[i];
-    if (c == '\r' || c == '\n' || c == ' ') {
-      continue;
-    }
-    bytes[pos++] = c;
-  }
-  return pos;
-}
-static inline size_t avx2_despace_branchless_u2(char *bytes, size_t howmany) {
-  size_t pos = 0;
-  __m128i spaces = _mm_set1_epi8(' ');
-  __m128i newline = _mm_set1_epi8('\n');
-  __m128i carriage = _mm_set1_epi8('\r');
-
-  __m256i spaces256 = _mm256_set1_epi8(' ');
-  __m256i newline256 = _mm256_set1_epi8('\n');
-  __m256i carriage256 = _mm256_set1_epi8('\r');
-
-  size_t i = 0;
-  for (; i + 64 - 1 < howmany; i += 64) {
-    __m256i x1, x2;
-    int offset11, offset12, offset21, offset22;
-    unsigned int masklow1, maskhigh1, masklow2, maskhigh2;
-
-    x1 = _mm256_loadu_si256((const __m256i *)(bytes + i));
-    x2 = _mm256_loadu_si256((const __m256i *)(bytes + i + 32));
-
-    x1 = cleanm256(x1,spaces256,newline256,carriage256, & masklow1, & maskhigh1);
-    offset11 = 16 - _mm_popcnt_u32(masklow1);
-    offset12 = 16 - _mm_popcnt_u32(maskhigh1);
-    x2 = cleanm256(x2,spaces256,newline256,carriage256, & masklow2, & maskhigh2);
-    offset21 = 16 - _mm_popcnt_u32(masklow2);
-    offset22 = 16 - _mm_popcnt_u32(maskhigh2);
-
-    _mm256_storeu2_m128i((__m128i *)(bytes + pos + offset11), (__m128i *)(bytes + pos ),x1);
-    pos += offset11 + offset12;
-
-    _mm256_storeu2_m128i((__m128i *)(bytes + pos + offset21), (__m128i *)(bytes + pos ),x2);
-    pos += offset21 + offset22;
-
-  }
-
-  for (; i + 32 - 1 < howmany; i += 32) {
-    unsigned int masklow, maskhigh;
-
-    int offset1, offset2;
-    __m256i x = _mm256_loadu_si256((const __m256i *)(bytes + i));
-    x = cleanm256(x,spaces256,newline256,carriage256, & masklow, & maskhigh);
-    offset1 = 16 - _mm_popcnt_u32(masklow);
-    offset2 = 16 - _mm_popcnt_u32(maskhigh);
-    _mm256_storeu2_m128i((__m128i *)(bytes + pos + offset1), (__m128i *)(bytes + pos ),x);
-    pos += offset1 + offset2;
-  }
-  for (; i + 16 - 1 < howmany; i += 16) {
-    __m128i x = _mm_loadu_si128((const __m128i *)(bytes + i));
-    int mask16;
-    x = cleanm128(x,spaces,newline,carriage, & mask16);
-    _mm_storeu_si128((__m128i *)(bytes + pos), x);
-    pos += 16 - _mm_popcnt_u32(mask16);
-  }
-  for (; i < howmany; i++) {
-    char c = bytes[i];
-    if (c == '\r' || c == '\n' || c == ' ') {
-      continue;
-    }
-    bytes[pos++] = c;
-  }
-  return pos;
-}
 
 
 static inline size_t sse4_despace_branchless_u4(char *bytes, size_t howmany) {
@@ -752,4 +542,221 @@ static inline size_t sse42_despace_branchless_lookup(char *bytes, size_t howmany
   return pos;
 }
 #endif
+
+
+
+
+#ifdef __AVX2__
+
+#include <x86intrin.h>
+
+
+#ifndef __clang__
+static inline __m256i _mm256_loadu2_m128i(__m128i const *__addr_hi, __m128i const *__addr_lo) {
+  __m256i __v256 = _mm256_castsi128_si256(_mm_loadu_si128(__addr_lo));
+  return _mm256_insertf128_si256(__v256, _mm_loadu_si128(__addr_hi), 1);
+}
+
+static inline void _mm256_storeu2_m128i(__m128i *__addr_hi, __m128i *__addr_lo, __m256i __a) {
+  __m128i __v128;
+
+  __v128 = _mm256_castsi256_si128(__a);
+  _mm_storeu_si128(__addr_lo, __v128);
+  __v128 = _mm256_extractf128_si256(__a, 1);
+  _mm_storeu_si128(__addr_hi, __v128);
+}
+#endif
+
+/**
+* remove spaces (in-place) from string bytes (UTF-8 or ASCII) containing
+* "howmany"
+* characters (not counting the null character if a C string is used), returns
+* the new number of characters
+* this function does add the null character, you have to do it yourself if you
+* need it.
+*/
+static inline size_t avx2_despace(char *bytes, size_t howmany) {
+  size_t pos = 0;
+  __m256i spaces = _mm256_set1_epi8(' ');
+  __m256i newline = _mm256_set1_epi8('\n');
+  __m256i carriage = _mm256_set1_epi8('\r');
+  size_t i = 0;
+  for (; i + 31 < howmany; i += 32) {
+    __m256i x = _mm256_loadu_si256((const __m256i *)(bytes + i));
+    // we do it the naive way, could be smarter?
+    __m256i xspaces = _mm256_cmpeq_epi8(x, spaces);
+    __m256i xnewline = _mm256_cmpeq_epi8(x, newline);
+    __m256i xcarriage = _mm256_cmpeq_epi8(x, carriage);
+    __m256i anywhite =
+        _mm256_or_si256(_mm256_or_si256(xspaces, xnewline), xcarriage);
+    if (_mm256_testz_si256(anywhite, anywhite) == 1) { // no white space
+      _mm256_storeu_si256((__m256i *)(bytes + pos), x);
+      pos += 32;
+    } else {
+      // do it the hard way
+      for (size_t j = 0; j < 32; j++) {
+        char c = bytes[i + j];
+        if (c == '\r' || c == '\n' || c == ' ') {
+          continue;
+        }
+        bytes[pos++] = c;
+      }
+    }
+  }
+  for (; i < howmany; i++) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      continue;
+    }
+    bytes[pos++] = c;
+  }
+  return pos;
+}
+
+static inline size_t avx2_countspaces(const char *bytes, size_t howmany) {
+  size_t count = 0;
+  __m256i spaces = _mm256_set1_epi8(' ');
+  __m256i newline = _mm256_set1_epi8('\n');
+  __m256i carriage = _mm256_set1_epi8('\r');
+  size_t i = 0;
+  for (; i + 31 < howmany; i += 32) {
+    __m256i x = _mm256_loadu_si256((const __m256i *)(bytes + i));
+    // we do it the naive way, could be smarter?
+    __m256i xspaces = _mm256_cmpeq_epi8(x, spaces);
+    __m256i xnewline = _mm256_cmpeq_epi8(x, newline);
+    __m256i xcarriage = _mm256_cmpeq_epi8(x, carriage);
+    __m256i anywhite =
+        _mm256_or_si256(_mm256_or_si256(xspaces, xnewline), xcarriage);
+    count += _mm_popcnt_u32(_mm256_movemask_epi8(anywhite));
+  }
+  for (; i < howmany; i++) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      count ++ ;
+    }
+  }
+  return count;
+}
+
+
+static inline __m256i cleanm256(__m256i x, __m256i spaces, __m256i newline, __m256i carriage, unsigned int * mask1, unsigned int *mask2) {
+    __m256i xspaces = _mm256_cmpeq_epi8(x, spaces);
+    __m256i xnewline = _mm256_cmpeq_epi8(x, newline);
+    __m256i xcarriage = _mm256_cmpeq_epi8(x, carriage);
+    __m256i anywhite = _mm256_or_si256(_mm256_or_si256(xspaces, xnewline), xcarriage);
+    unsigned int mask32 = _mm256_movemask_epi8(anywhite);
+    unsigned int maskhigh = (mask32) >> 16;
+    unsigned int masklow = (mask32) & 0xFFFF;
+    assert(maskhigh<(1<<16));
+    assert(masklow<(1<<16));
+    *mask1 = masklow;
+    *mask2 = maskhigh;
+    __m256i mask = _mm256_loadu2_m128i((const __m128i *)despace_mask16 + maskhigh, (const __m128i *)despace_mask16 + masklow);
+    return _mm256_shuffle_epi8(
+          x, mask);
+
+}
+
+static inline size_t avx2_despace_branchless(char *bytes, size_t howmany) {
+  size_t pos = 0;
+  __m128i spaces = _mm_set1_epi8(' ');
+  __m128i newline = _mm_set1_epi8('\n');
+  __m128i carriage = _mm_set1_epi8('\r');
+
+  __m256i spaces256 = _mm256_set1_epi8(' ');
+  __m256i newline256 = _mm256_set1_epi8('\n');
+  __m256i carriage256 = _mm256_set1_epi8('\r');
+
+  size_t i = 0;
+  for (; i + 32 - 1 < howmany; i += 32) {
+    __m256i x = _mm256_loadu_si256((const __m256i *)(bytes + i));
+    unsigned int masklow, maskhigh;
+    x = cleanm256(x,spaces256,newline256,carriage256, & masklow, & maskhigh);
+    int offset1 = 16 - _mm_popcnt_u32(masklow);
+    int offset2 = 16 - _mm_popcnt_u32(maskhigh);
+    _mm256_storeu2_m128i((__m128i *)(bytes + pos + offset1), (__m128i *)(bytes + pos ),x);
+    pos += offset1 + offset2;
+  }
+  for (; i + 16 - 1 < howmany; i += 16) {
+    __m128i x = _mm_loadu_si128((const __m128i *)(bytes + i));
+    int mask16;
+    x = cleanm128(x,spaces,newline,carriage, & mask16);
+    _mm_storeu_si128((__m128i *)(bytes + pos), x);
+    pos += 16 - _mm_popcnt_u32(mask16);
+  }
+  for (; i < howmany; i++) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      continue;
+    }
+    bytes[pos++] = c;
+  }
+  return pos;
+}
+static inline size_t avx2_despace_branchless_u2(char *bytes, size_t howmany) {
+  size_t pos = 0;
+  __m128i spaces = _mm_set1_epi8(' ');
+  __m128i newline = _mm_set1_epi8('\n');
+  __m128i carriage = _mm_set1_epi8('\r');
+
+  __m256i spaces256 = _mm256_set1_epi8(' ');
+  __m256i newline256 = _mm256_set1_epi8('\n');
+  __m256i carriage256 = _mm256_set1_epi8('\r');
+
+  size_t i = 0;
+  for (; i + 64 - 1 < howmany; i += 64) {
+    __m256i x1, x2;
+    int offset11, offset12, offset21, offset22;
+    unsigned int masklow1, maskhigh1, masklow2, maskhigh2;
+
+    x1 = _mm256_loadu_si256((const __m256i *)(bytes + i));
+    x2 = _mm256_loadu_si256((const __m256i *)(bytes + i + 32));
+
+    x1 = cleanm256(x1,spaces256,newline256,carriage256, & masklow1, & maskhigh1);
+    offset11 = 16 - _mm_popcnt_u32(masklow1);
+    offset12 = 16 - _mm_popcnt_u32(maskhigh1);
+    x2 = cleanm256(x2,spaces256,newline256,carriage256, & masklow2, & maskhigh2);
+    offset21 = 16 - _mm_popcnt_u32(masklow2);
+    offset22 = 16 - _mm_popcnt_u32(maskhigh2);
+
+    _mm256_storeu2_m128i((__m128i *)(bytes + pos + offset11), (__m128i *)(bytes + pos ),x1);
+    pos += offset11 + offset12;
+
+    _mm256_storeu2_m128i((__m128i *)(bytes + pos + offset21), (__m128i *)(bytes + pos ),x2);
+    pos += offset21 + offset22;
+
+  }
+
+  for (; i + 32 - 1 < howmany; i += 32) {
+    unsigned int masklow, maskhigh;
+
+    int offset1, offset2;
+    __m256i x = _mm256_loadu_si256((const __m256i *)(bytes + i));
+    x = cleanm256(x,spaces256,newline256,carriage256, & masklow, & maskhigh);
+    offset1 = 16 - _mm_popcnt_u32(masklow);
+    offset2 = 16 - _mm_popcnt_u32(maskhigh);
+    _mm256_storeu2_m128i((__m128i *)(bytes + pos + offset1), (__m128i *)(bytes + pos ),x);
+    pos += offset1 + offset2;
+  }
+  for (; i + 16 - 1 < howmany; i += 16) {
+    __m128i x = _mm_loadu_si128((const __m128i *)(bytes + i));
+    int mask16;
+    x = cleanm128(x,spaces,newline,carriage, & mask16);
+    _mm_storeu_si128((__m128i *)(bytes + pos), x);
+    pos += 16 - _mm_popcnt_u32(mask16);
+  }
+  for (; i < howmany; i++) {
+    char c = bytes[i];
+    if (c == '\r' || c == '\n' || c == ' ') {
+      continue;
+    }
+    bytes[pos++] = c;
+  }
+  return pos;
+}
+
+
+#endif
+
+
 #endif
