d021f5442eb94b2bb3f5f2592dbc1178699f50eb
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-09 08:46:59 -0500

    Remove inline so the symbols are exported from the static library

--- include/despacer_SSE4_2.h
+++ include/despacer_SSE4_2.h
@@ -2,12 +2,12 @@
 
 #include <x86intrin.h>
 
-inline size_t sse42_despace_to(const char *__restrict__ bytes, size_t howmany, char *__restrict__ out);
+size_t sse42_despace_to(const char *__restrict__ bytes, size_t howmany, char *__restrict__ out);
 
-inline size_t sse42_despace(char *bytes, size_t howmany);
+size_t sse42_despace(char *bytes, size_t howmany);
 
-inline size_t sse42_despace_branchless(char *bytes, size_t howmany);
+size_t sse42_despace_branchless(char *bytes, size_t howmany);
 
-inline size_t sse42_despace_branchless_lookup(char *bytes, size_t howmany);
+size_t sse42_despace_branchless_lookup(char *bytes, size_t howmany);
 
 #endif
--- include/despacer_generic.h
+++ include/despacer_generic.h
@@ -10,26 +10,26 @@ size_t despace (char * bytes, size_t howmany);
 
 /// make the store unconditional
 // credit: https://github.com/aqrit
-inline size_t despace_branchless (void * dst_void, void * src_void, size_t length);
+size_t despace_branchless (void * dst_void, void * src_void, size_t length);
 
 /// explicit branchless
 // credit: https://github.com/aqrit
-inline size_t despace_cmov (void * dst_void, void * src_void, size_t length);
+size_t despace_cmov (void * dst_void, void * src_void, size_t length);
 
 // credit: https://github.com/aqrit
-inline size_t despace_table (void * dst_void, void * src_void, size_t length);
+size_t despace_table (void * dst_void, void * src_void, size_t length);
 
-inline size_t despace32 (char * bytes, size_t howmany);
+size_t despace32 (char * bytes, size_t howmany);
 
-inline size_t faster_despace (char * bytes, size_t howmany);
+size_t faster_despace (char * bytes, size_t howmany);
 
-inline size_t faster_despace32 (char * bytes, size_t howmany);
+size_t faster_despace32 (char * bytes, size_t howmany);
 
-inline size_t countspaces (char const * bytes, size_t howmany);
+size_t countspaces (char const * bytes, size_t howmany);
 
-inline size_t countspaces32 (char const * bytes, size_t howmany);
+size_t countspaces32 (char const * bytes, size_t howmany);
 
-inline size_t despace64 (char * bytes, size_t howmany);
+size_t despace64 (char * bytes, size_t howmany);
 
 /**
 * remove spaces from string bytes (UTF-8 or ASCII) containing "howmany"
@@ -40,4 +40,4 @@ inline size_t despace64 (char * bytes, size_t howmany);
 * Result is stored in "out", where the caller is responsible for memory
 * allocation.
 */
-inline size_t despace_to (char const * bytes, size_t howmany, char * out);
+size_t despace_to (char const * bytes, size_t howmany, char * out);
