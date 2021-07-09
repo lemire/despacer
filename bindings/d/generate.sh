b5f6fdb103d62ca0ea3bd50986028780f9f1fead
Amin Yahyaabadi <aminyahyaabadi74@gmail.com>
2021-07-09 17:35:42 -0500

    Add -march=native to dpp generation

--- bindings/d/generate.sh
+++ bindings/d/generate.sh
@@ -1,13 +1,7 @@
 cd ./src/libc
 
 # Generate Bindings using dpp
-dub run --build=release --quiet --yes dpp --  "./despacer_generic.dpp" "./despacer_SSSE3.dpp" "./despacer_SSE4_1.dpp" "./despacer_SSE4_2.dpp" --ignore-system-paths --inlcude-path "x86intrin.h" --preprocess-only --define __SSSE3__ --define __SSE4_1__ --define __SSE4_2__ # --define __AVX2__
-
-{
-  dub run --build=release --quiet --yes dpp -- "./despacer_AVX2.dpp" --ignore-system-paths --inlcude-path "x86intrin.h" --preprocess-only --define __AVX2__
-} || {
-  echo "AVX2 is not supported on this system"
-}
+dub run --build=release --quiet --yes dpp --  "./despacer_generic.dpp" "./despacer_SSSE3.dpp" "./despacer_SSE4_1.dpp" "./despacer_SSE4_2.dpp" "./despacer_AVX2.dpp" --ignore-system-paths --inlcude-path "x86intrin.h" --preprocess-only --clang-option "-march=native"
 
 cd ../..
 
